a6248a14062fa85ec811904367f07b1e5cc59450
Maxime Mangel <maxime.shadaen@protonmail.com>
2018-06-25 17:36:52 +0200

    WIP

--- demos/Thoth.Elmish.Demo/src/App.fs
+++ demos/Thoth.Elmish.Demo/src/App.fs
@@ -2,10 +2,12 @@ module Demos.Main
 
 type IExposeApi =
     abstract DebouncerDemo: string -> unit
+    abstract ToastDemo: string -> unit
 
 let exposeApi =
     { new IExposeApi with
         member __.DebouncerDemo (id :string) = Debouncer.start id
+        member __.ToastDemo (id :string) = Toast.start id
     }
 open Fable.Core.JsInterop
 
